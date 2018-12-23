c9c27f456f7c3b805413789147dcf1b2a8ca3a09
leeyee <seadlead@gmail.com>
2018-12-23 21:49:51 +0800

    ES6

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,6 +1,6 @@
 /**
  * 俄罗斯方块所在画布类
- * 
+ *
  * @author leeyee
  * @blog blog.csdn.net/oxcow
  * @email seadlead@gmail.com
@@ -12,28 +12,32 @@
  * @param sOffset
  *            画布显示位置
  */
-function Canvas(iRow, iColumn, sOffset) {
-	this.row = iRow;// 行
-	this.column = iColumn;// 列
-	this.offset = sOffset;// 显示位置
+class Canvas {
+    constructor(iRow, iColumn, sOffset) {
+        this.row = iRow;// 行
+        this.column = iColumn;// 列
+        this.offset = sOffset;// 显示位置
+    }
+
+    draw() {
+        let oDiv_canvas = document.getElementById(this.offset);
+        if (oDiv_canvas == null) {
+            oDiv_canvas = Utils.CreateElement('div', this.offset, null);
+            document.body.appendChild(oDiv_canvas);
+        }
+        let oFragment = document.createDocumentFragment();// 创建文档碎片
+        for (let i = 1; i <= this.row * this.column; i++) {
+            let oSpan = Utils.CreateElement('span', i, i);
+            oFragment.appendChild(oSpan);
+        }
+        oDiv_canvas.appendChild(oFragment);
+    }
+
 }
-Canvas.prototype.draw = function() {
-	var oDiv_canvas = document.getElementById(this.offset);
-	if (oDiv_canvas == null) {
-		oDiv_canvas = createElement("div", this.offset, null);
-		document.body.appendChild(oDiv_canvas);
-	}
-	var oFragment = document.createDocumentFragment();// 创建文档碎片
-	for ( var i = 1; i <= this.row * this.column; i++) {
-		var oSpan = createElement("span", i, null);
-		oFragment.appendChild(oSpan);
-	}
-	oDiv_canvas.appendChild(oFragment);
-};
 
 /**
  * 预览画布类
- * 
+ *
  * @param iRow
  *            行数
  * @param iColumn
@@ -42,35 +46,39 @@ Canvas.prototype.draw = function() {
  *            显示位置
  * @return
  */
-function PreCanvas(iRow, iColumn, sOffset) {
-	this.row = iRow;
-	this.column = iColumn;
-	this.offset = sOffset;
+class PreCanvas {
+    constructor(iRow, iColumn, sOffset) {
+        this.row = iRow;
+        this.column = iColumn;
+        this.offset = sOffset;
+    }
+
+    draw() {
+        let oDiv_preCanvas = document.getElementById(this.offset);
+        if (oDiv_preCanvas == null) {
+            oDiv_preCanvas = Utils.CreateElement('div', this.offset, null);
+            document.body.appendChild(oDiv_preCanvas);
+        }
+        let oFragment = document.createDocumentFragment();// 创建文档碎片
+        for (let i = -(this.row * this.column) + 11; i < 11; i++) {
+            let oSpan = Utils.CreateElement('span', `pre_${i}`, null);
+            oFragment.appendChild(oSpan);
+        }
+        oDiv_preCanvas.appendChild(oFragment);
+    }
+
+    show(oArray) {
+        for (let i = -(this.row * this.column) + 11; i < 11; i++) {
+            let __oE = document.getElementById(`pre_${i}`);
+            if (__oE) {
+                __oE.style.background = document.getElementById(this.offset).style.background;
+            }
+        }
+        for (let i = 0; i < oArray.length; i++) {
+            let __oE = document.getElementById(`pre_${oArray[i]}`);
+            if (__oE) {
+                __oE.style.background = 'blue';
+            }
+        }
+    }
 }
-PreCanvas.prototype.draw = function() {
-	var oDiv_preCanvas = document.getElementById(this.offset);
-	if (oDiv_preCanvas == null) {
-		oDiv_preCanvas = createElement("div", this.offset, null);
-		document.body.appendChild(oDiv_preCanvas);
-	}
-	var oFragment = document.createDocumentFragment();// 创建文档碎片
-	for ( var i = -(this.row * this.column) + 11; i < 11; i++) {
-		var oSpan = createElement("span", "pre_" + i, null);
-		oFragment.appendChild(oSpan);
-	}
-	oDiv_preCanvas.appendChild(oFragment);
-};
-PreCanvas.prototype.show = function(oArray) {
-	for ( var i = -(this.row * this.column) + 11; i < 11; i++) {
-		var __oE = document.getElementById("pre_" + i);
-		if (__oE) {
-			__oE.style.background = document.getElementById(this.offset).style.background;
-		}
-	}
-	for ( var i = 0; i < oArray.length; i++) {
-		var __oE = document.getElementById("pre_" + oArray[i]);
-		if (__oE) {
-			__oE.style.background = "blue";
-		}
-	}
-};
--- src/Utils.js
+++ src/Utils.js
@@ -5,113 +5,77 @@
  * @email seadlead@gmail.com
  * @data 2010-4
  */
-/**
- * 创建DOM元素
- * 
- * @param sTagName
- *            元素名
- * @param sTagId
- *            元素ID
- * @param sText
- *            元素文本
- * @return DOM元素对象
- */
-function createElement(sTagName, sTagId, sText) {
-	var oElement = document.createElement(sTagName);
-	if (sTagId != null) {
-		oElement.setAttribute("id", sTagId);
-	}
-	if (sText != null) {
-		oElement.appendChild(document.createTextNode(sText));
-	}
-	return oElement;
+
+
+class Utils {
+    /**
+     * 创建DOM元素
+     *
+     * @param sTagName
+     *            元素名
+     * @param sTagId
+     *            元素ID
+     * @param sText
+     *            元素文本
+     * @return HTMLElement 元素对象
+     */
+    static CreateElement(sTagName, sTagId, sText) {
+        let oElement = document.createElement(sTagName);
+        if (sTagId != null) {
+            oElement.setAttribute('id', sTagId);
+        }
+        if (sText != null) {
+            oElement.appendChild(document.createTextNode(sText));
+        }
+        return oElement;
+    }
+
+    /**
+     * 创建一个长度为 iLen 的二维数组，其中第二位数组的长度未定义
+     *
+     * @param iLen
+     *            数组长度
+     * @return
+     */
+    static CreateTwoDimensionArray(iLen) {
+        let __aDefaultArray = new Array(iLen);
+        for (let i = 0; i < iLen; i++) {
+            __aDefaultArray[i] = [];
+        }
+        return __aDefaultArray;
+    }
+
+    /**
+     * 将目标数组 aObj 以 iN 分组。具有相同商的放在相同维度中，同时根据余数确定二维存放位置<br/>
+     *
+     * 比如将 [1,3,21] 以10分组，那么返回的二维数组为：[[1,,3],[],[21]]
+     *
+     * @param aObj
+     *            目标数组
+     * @param iN
+     *            分组因子
+     * @return Array一个长度为20的二维数组
+     */
+    static ElementGroupByN(aObj, iN) {
+        let __aZeroArray = Utils.CreateTwoDimensionArray(20, []);
+        for (let i = 0; i < aObj.length; i++) {
+            let __quotient = Math.floor((aObj[i] - 1) / iN);// 取商(被10除,商相同的元素为一组)
+            let __remainder = (aObj[i] - 1) % iN;// 取余
+            __aZeroArray[__quotient][__remainder] = aObj[i];// 商相同的放在相同维度的余数位置
+        }
+        return __aZeroArray;
+    }
+
+    /**
+     * 得到从零到N的随机整数
+     *
+     * @param iN
+     *            最大整数
+     * @return Number 0-iN的随机整数
+     */
+    static RandomNumFromZeroToN(iN) {
+        let iR = Math.random() * iN;
+        return Math.round(iR);// 舍尾取数
+    }
 }
-function RandomEventUtils() {
-}
-/**
- * 得到从零到N的随机整数
- * 
- * @param iN
- *            最大整数
- * @return 0-iN的随机整数
- */
-RandomEventUtils.getIntRandomNumFromZeroToN = function(iN) {
-	var iR = Math.random() * iN;
-	return Math.round(iR);// 舍尾取数
-};
-/**
- * 获取某个元素在数组中的位置
- * 
- * @param vItem
- *            目标元素
- * @return 目标元素在数组中的索引
- */
-Array.prototype.indexOf = function(vItem) {
-	for ( var i = 0; i < this.length; i++) {
-		if (vItem == this[i]) {
-			return i;
-		}
-	}
-	return -1;
-};
-/**
- * 获取数组的长度。不包括undefined元素
- * 
- * @return
- */
-Array.prototype._length = function() {
-	var __len = 0;
-	for ( var i = 0; i < this.length; i++) {
-		if (this[i] != undefined) {
-			__len++;
-		}
-	}
-	return __len;
-};
-/**
- * 清除数组中的undefined元素
- * 
- * @return 数组清除后的数组（数组长度改变）
- */
-Array.prototype.cleanUndefinedElement = function() {
-	for ( var i = 0; i < this.length; i++) {
-		if (this[i] == undefined) {
-			this.splice(i--, 1);
-		}
-	}
-	return this;
-};
-/**
- * 创建一个长度为 iLen 的二维数组，其中第二位数组的长度未定义
- * 
- * @param iLen
- *            数组长度
- * @return
- */
-function createTwoDimensionArray(iLen) {
-	var __aDefaultArray = new Array(iLen);
-	for ( var i = 0; i < iLen; i++) {
-		__aDefaultArray[i] = new Array();
-	}
-	return __aDefaultArray;
-}
-/**
- * 将目标数组 aObj 以 iN 分组。具有相同商的放在相同维度中，同时根据余数确定二维存放位置<br/>
- * 
- * 比如将 [1,3,21] 以10分组，那么返回的二维数组为：[[1,,3],[],[21]]
- * 
- * @param aObj
- *            目标数组
- * @param iN
- *            分组因子
- * @return 一个长度为20的二维数组
- */
-function elementGroupByN(aObj, iN) {
-	var __aZeroArray = createTwoDimensionArray(20, new Array());
-	for ( var i = 0; i < aObj.length; i++) {
-		var __quotient = Math.floor((aObj[i] - 1) / iN);// 取商(被10除,商相同的元素为一组)
-		var __remainder = (aObj[i] - 1) % iN;// 取余
-		__aZeroArray[__quotient][__remainder] = aObj[i];// 商相同的放在相同维度的余数位置
-	}
-	return __aZeroArray;
-}
+
